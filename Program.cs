2a560dd1ea6baae4b5dfe2df91ce6103e0ae2aa9
sagar <skatahre@gmail.com>
2022-10-27 19:07:04 +0530

    find max string value

--- Program.cs
+++ Program.cs
@@ -5,11 +5,14 @@
     {
         static void Main(string[] args)
         {
+
             //FindMaxNum findMaxNum = new FindMaxNum();
             //findMaxNum.MaximumNum(10, 20, 30);
+            //FindMaxFloat findMaxfloat = new FindMaxFloat();
+            //findMaxfloat.MaximumFloat(33.6, 96.5, 7.8);
 
-            FindMaxFloat findMaxfloat = new FindMaxFloat();
-            findMaxfloat.MaximumFloat(33.6, 6.5, 7.8);
+            FindMaxString findMaxstring = new FindMaxString();
+            findMaxstring.MaximumString("ball", "hello", "bye");
         }
     }
 }
