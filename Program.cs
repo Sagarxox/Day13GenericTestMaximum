f6617318fa7b98ae6cd58f0ee82e0a93878c3560
sagar <skatahre@gmail.com>
2022-10-28 00:27:23 +0530

    find max string value using generics

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Day13Generics
+﻿using System;
+
+namespace Day13Generics
 
 {
     public class Program
@@ -6,13 +8,17 @@
         static void Main(string[] args)
         {
 
-            //FindMaxNum findMaxNum = new FindMaxNum();
-            //findMaxNum.MaximumNum(10, 20, 30);
-            //FindMaxFloat findMaxfloat = new FindMaxFloat();
-            //findMaxfloat.MaximumFloat(33.6, 96.5, 7.8);
-
-            FindMaxString findMaxstring = new FindMaxString();
-            findMaxstring.MaximumString("ball", "hello", "bye");
+            Console.WriteLine("Find Maximum of given inputs");
+            int[] intArray = { 190, 521, 607, 400, 20 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+            generic.PrintMaxValue();
+            double[] doubleArray = { 40.2, 3.4, 6.2, 75.5, 5.86 };
+            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+            genericDouble.PrintMaxValue();
+            string[] stringArray = { "CAMERA", "HELLO", "CAT", "BIKE", "WINDOW" };
+            GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
+            genericString.PrintMaxValue();
+            Console.ReadLine();
         }
     }
 }
